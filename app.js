02980de3fa3044810e231be77fb84222ff518b59
unknown <victor.sampaio2003@gmail.com>
2023-07-04 18:02:34 -0300

    corrigindo app js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Montar o cálculo da fórmula anotada no caderno
 
-let valorDaTaxa = (saldoRestante * taxaDoCartao) / (taxaDoCartao - 1);
 let taxaDoCartao = 0.0261;
 let saldoRestante = 1000;
+let valorDaTaxa = (saldoRestante * taxaDoCartao) / (taxaDoCartao - 1);
 let valorPedido = saldoRestante + (-valorDaTaxa);
 
 console.log("O valor da taxa é:", valorDaTaxa);
