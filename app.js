96d27d833e4b9ffe271539dc29119e83d64aa46f
Victor Sampaio <victor.sampaio2003@gmail.com>
2023-07-07 15:16:25 -0300

    ajustando app js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //Montar o cálculo da fórmula anotada no caderno
-
-let taxaDoCartao = prompt("Digite a taxa do cartão em números DESCIMAIS");
+alert ("Digite os números com '.' no lugar das vírgulas")
+let taxaDoCartao = prompt("Digite a taxa do cartão");
 let saldoRestante = prompt("Digite o saldo restante do pedido");
+
+taxaDoCartao = parseFloat(taxaDoCartao);
+saldoRestante = parseFloat(saldoRestante);
+
 let valorDaTaxa = (saldoRestante * taxaDoCartao) / (taxaDoCartao - 1);
 let valorPedido = saldoRestante + (-valorDaTaxa);
 
-alert("O valor da taxa é:", valorDaTaxa);
-alert("O valor do pedido é:", valorPedido);
+alert("O valor da taxa é:" + valorDaTaxa.toFixed(2));
+alert("O valor do pedido é:" + valorPedido.toFixed(2));
